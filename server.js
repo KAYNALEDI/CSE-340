f833623164d3c7cf65b0987b5ef2d4459be3307e
KAYNALEDI <nokwazipalesa@gmail.com>
2024-07-08 21:38:23 -0600

    server

--- server.js
+++ server.js
@@ -5,18 +5,18 @@
 /* ***********************
  * Require Statements
  *************************/
-const expressLayouts = require("express-ejs-layouts")
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts");
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
 
 /* ***********************
  *View Engine and Templates
  *************************/
- app.set("view engine", "ejs")
- app.use(expressLayouts)
- app.set("layout", "./layouts/layout") // not at views root
+ app.set("view engine", "ejs");
+ app.use(expressLayouts);
+ app.set("layout", "./layouts/layout"); // not at views root
  
 
 
@@ -26,21 +26,52 @@ const static = require("./routes/static")
 app.use(static)
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-  })
+// app.get("/", function(req, res){
+//   res.render("index", {title: "Home"})
+//   })
+
+app.get("/", utilities.handleErrors(baseController.buildHome));
+// Inventory routes
+app.use("/inv", inventoryRoute);
+// Account routes
+app.use("/account", accountRoute);
+// Message routes
+app.use("/message", messageRoute);
+// Intentional error route. Used for testing
+app.use("/ierror", intentionalErrorRoute);
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Out of stock, we don\'t have that page in our inventory.'});
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  console.dir(err);
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav,
+    errors:null,
+  });
+});
 
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on ${host}:${port}`);
 })
